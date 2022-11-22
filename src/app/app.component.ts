4a086f94227e2e813a9a3e5ca6609942a1637238
Leonardo <l.guedes@ges.inatel.br>
2022-11-21 21:31:16 -0300

    fiz nada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Tone from 'tone'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ForgiveMeFatherIHaveSynthed';
+
+  ngOnInit() {
+
+    document.addEventListener('keydown', this.playSound)
+
+  }
+
+  playSound(event: any) {
+    let nota: string = event.key;
+    var scales: any = {
+      "g": { "f": "f#" }
+    }
+
+    //tocar nota;;
+    const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+    const now = Tone.now()
+    let scale = scales["g"];
+    let note = (scale[nota] ? scale[nota] : nota).toUpperCase()+4;
+    synth.triggerAttack(note, now)
+    synth.triggerRelease([note], now + 1)
+
+
+
+  }
 }
 
- 
+
+
+
