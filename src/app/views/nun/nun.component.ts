e24ccec5bab2c2129535d728e2f071d5ef2f7d61
Leonardo <l.guedes@ges.inatel.br>
2022-11-24 18:30:36 -0300

    new changes

--- src/app/views/nun/nun.component.ts
+++ src/app/views/nun/nun.component.ts
@@ -6,26 +6,52 @@ import * as Tone from 'tone';
   templateUrl: './nun.component.html',
   styleUrls: ['./nun.component.scss']
 })
-export class NunComponent implements OnInit {
 
-  constructor() { }
+export class NunComponent implements OnInit {
 
   ngOnInit(): void {
     document.addEventListener('keydown', this.playKey)
   }
 
   playKey(event: any): void {
-    var key: HTMLElement = event.target;
-    let note: string;// = this.keyMap[event.key];
-    if (event.type === 'click')
-      note = this.keyMap[key.innerText.toLowerCase()];
-    else
-      note = this.keyMap[event.key];
+    let keyMap: any = {
+      //white-keys
+      "q": "C4", "w": "D4", "e": "E4", "r": "F4", "t": "G4", "y": "A4", "u": "B4",
+      "i": "C5", "o": "D5", "p": "E5", "a": "F5", "s": "G5", "d": "A5", "f": "B5",
+      //black-keys
+      "0": "C#4", "1": "D#4", "2": "F#4", "3": "G#4", "4": "A#4",
+      "5": "C#5", "6": "D#5", "7": "F#5", "8": "G#5", "9": "A#5"
+    }
+
+    var key: any = null;
+    let note: string;
+
+    if (event.type === 'click'){
+      key = event.target;
+      var value = (key.textContent + "").toLowerCase()
+      note = keyMap[value??"q"];
+    }
+    else {
+      note = keyMap[event.key];
+
+      var aTags = document.getElementsByClassName("key");
+      var searchText = event.key.toUpperCase();
+
+      for (var i = 0; i < aTags.length; i++) {
+        if (aTags[i].textContent == searchText) {
+          key = aTags[i];
+          break;
+        }
+      }
+    }
+
     //Mover mao
-    var keyX = key.getBoundingClientRect().left;
-    var hand = document.getElementById("hand");
-    if (hand != null)
-      hand.style.left = keyX - 50 + "px";
+    if(key != null){
+      var keyX = key.getBoundingClientRect().left;
+      var hand = document.getElementById("hand");
+      if (hand != null)
+      hand.style.left = keyX - 30 + "px";
+    }
 
     //Tocar nota
     const synth = new Tone.PolySynth(Tone.Synth).toDestination();
@@ -34,14 +60,11 @@ export class NunComponent implements OnInit {
     synth.triggerRelease([note], now + 1)
   }
 
-  keyMap: any = {
-    //white-keys
-    "q": "C4", "w": "D4", "e": "E4", "r": "F4", "t": "G4", "y": "A4", "u": "B4",
-    "i": "C5", "o": "D5", "p": "E5", "a": "F5", "s": "G5", "d": "A5", "f": "B5",
-    //black-keys
-    "0": "C#4", "1": "D#4", "2": "F#4", "3": "G#4", "4": "A#4",
-    "5": "C#5", "6": "D#5", "7": "F#5", "8": "G#5", "9": "A#5"
+  playMeow(){
+    var meow = new Audio("../../../assets/imgs/meow.wav");
+    meow.play();
   }
+
 }
 
 
